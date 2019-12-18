3254e4f7fa3d39f77164ae6a74976a2eec303e9e
FerBeaujean <joey732@hotmail.com>
2019-12-17 18:14:14 -0600

    hice la pagina web

---
+++ style.js
@@ -0,0 +1,17 @@
+let input= document.getElementById (opcion1)
+
+function myFunction() {
+    // var x = document.getElementById("opcion1");
+    // x.value = x.value.toUpperCase();
+    let didi= document.getElementById ("precio1")
+    let beat= document.getElementById ("precio2")
+    let uber= document.getElementById ("precio3")
+    let cabify= document.getElementById ("precio4")
+
+    // console.log(didi.innerHTML, beat.innerHTML, uber.innerHTML, cabify.innerHTML);
+    didi.innerHTML = "$ 51.50";
+    beat.innerHTML = "$ 60.73";
+    uber.innerHTML = "$ 75.19";
+    cabify.innerHTML = "$ 76.20";
+  }
+
